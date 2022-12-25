bf74e3e5373a44913b1193aa1570d212f6abbb47
juan <juan@MSI.home>
2022-12-25 20:01:39 +0100

    El Wite-box testing

--- src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertIntTestTest.java
+++ src/test/java/es/upm/grise/profundizacion/whiteboxtesting/Test_convertIntTestTest.java
@@ -1,7 +1,7 @@
 package es.upm.grise.profundizacion.whiteboxtesting;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 import org.junit.jupiter.api.Test;
 
